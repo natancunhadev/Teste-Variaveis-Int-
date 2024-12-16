d02cb9f0535a1903726e727a03855aa587650e91
Natan Cunha <104222003+natancunhadev@users.noreply.github.com>
2024-12-16 10:49:32 -0800

    Add files via upload

---
+++ teste variaveis int.py
@@ -0,0 +1,19 @@
+
+
+#inteiro: int
+#python
+
+idade = 38
+ano = 2024
+hora = 19
+minuto = 57 
+ 
+print(idade)
+print(type(idade))
+print("Ano:", ano, "/ Tipo:", type(ano))
+print("Hora:", hora, "/ Tipo:", type(hora))
+print("Minute:", minuto, "\ Tipo:", type(minuto))
+
+
+
+#teste variaveis int by Natan Cunha 2024
